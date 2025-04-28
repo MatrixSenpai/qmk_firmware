1d4f16c80fe7d7736a6c84a31132d14b184cb5b2
MatrixSenpai <math.matrix@icloud.com>
2025-04-27 22:43:50 -0500

    minor update

--- keyboards/sofle_split_matrix/keymaps/default/keymap.c
+++ keyboards/sofle_split_matrix/keymaps/default/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   					     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   					     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,		KC_F12,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
-        			  	  KC_LGUI, KC_LALT, KC_SPC,  KC_M,    KC_I,  		DF(_QL), KC_SPC,  KC_MINS, KC_LBRC, KC_RBRC
+        			  	  KC_LGUI, KC_LALT, KC_SPC,  KC_M,    KC_I,  		DF(_QL), KC_SPC,  KC_DOWN, KC_UP,   KC_RGHT
     ),
 };
 
