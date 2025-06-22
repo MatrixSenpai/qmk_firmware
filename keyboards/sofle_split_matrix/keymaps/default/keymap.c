56c403d159d2ce50341f2100d4a9709b46e290b9
MatrixSenpai <math.matrix@icloud.com>
2025-06-21 23:02:20 -0500

    pushing for support

--- keyboards/sofle_split_matrix/keymaps/default/keymap.c
+++ keyboards/sofle_split_matrix/keymaps/default/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "basic_profiling.h"
 #include "leader.c"
 #include "ux.c"
 
@@ -59,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         			  	  KC_LGUI, KC_LALT, TT(_AL),  QK_LEAD, KC_SPC,		DF(_GL), KC_SPC,  KC_MINS, KC_LBRC, KC_RBRC
     ),
     [_AL] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,	     				     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,	     				 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
         KC_TRNS, KC_F11,  KC_F12,  KC_F13,  KC_F14,   KC_F15,					     KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,					     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_BSLS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -85,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_F11,  KC_F12,  KC_F13,  KC_F14,   KC_F15,					     KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,					     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_BSLS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        			  	  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,		KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS
+        			  	  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,		KC_TRNS, KC_PSCR, KC_TRNS, KC_LBRC, KC_RBRC
     ),
 };
 
@@ -100,6 +101,11 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif
 
 bool oled_task_user(void) {
+    bool result;
+    PROFILE_CALL_NAMED(1000, "oled_task_user", { result = oled_task_user_impl(); });
+    return result;
+}
+void oled_task_user_impl(void) {
     if(is_keyboard_master()) {
         oled_render_anim();
     } else {
